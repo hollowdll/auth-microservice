32942adf86d0804fe6dde47f6ec5a981f6f007d6
hollowdll <pixerchat@gmail.com>
2023-11-10 22:05:04 +0200

    configure db and identity

--- src/AuthService/Config/Config.cs
+++ src/AuthService/Config/Config.cs
@@ -1,3 +1,5 @@
+namespace AuthService.Config;
+
 // Application configurations
 public class Config
 {
--- src/AuthService/Program.cs
+++ src/AuthService/Program.cs
@@ -1,9 +1,20 @@
+using AuthService.Data;
+using AuthService.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configure database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("UserDatabase")));
+
+// Configure identity system
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
